1d7e9f0a002111ea451c8af1bd9cbd85e2600255
guilhermecsr <guicsramos@gmail.com>
2021-11-16 12:50:04 -0300

    imagem redimensiona de acordo com o tamanho da janela

--- mycanvas.py
+++ mycanvas.py
@@ -4,56 +4,57 @@ from OpenGL.GL import *
 
 
 class MyCanvas(QtOpenGL.QGLWidget):
+
     def __init__(self):
         super(MyCanvas, self).__init__()
         self.m_model = None
         self.m_w = 0  # width: GL canvas horizontal size
         self.m_h = 0  # height: GL canvas vertical size
+        self.m_L = -1000.0
+        self.m_R = 1000.0
+        self.m_B = -1000.0
+        self.m_T = 1000.0
+        self.list = None
 
     def initializeGL(self):
         # glClearColor(1.0, 1.0, 1.0, 1.0)
         glClear(GL_COLOR_BUFFER_BIT)
+        self.list = glGenLists(1)
 
     def resizeGL(self, _width, _height):
-        # store GL canvas sizes in object properties
         self.m_w = _width
         self.m_h = _height
-        # setup the viewport to canvas dimensions
+        if(self.m_model==None)or(self.m_model.isEmpty()): self.scaleWorldWindow(1.0)
+        else:
+            self.m_L,self.m_R,self.m_B,self.m_T = self.m_model.getBoundBox()
+            self.scaleWorldWindow(1.1)
         glViewport(0, 0, self.m_w, self.m_h)
-        # reset the coordinate system
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        # establish the clipping volume by setting up an
-        # orthographic projection
-        glOrtho(0.0, self.m_w, 0.0, self.m_h, -1.0, 1.0)
-        # setup display in model coordinates
+        glOrtho(self.m_L,self.m_R,self.m_B,self.m_T,-1.0,1.0)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
 
     def paintGL(self):
-        # clear the buffer with the current clear color
         glClear(GL_COLOR_BUFFER_BIT)
-        # draw the model
-        if (self.m_model==None)or(self.m_model.isEmpty()):
-            return
-        # Display model polygon RGB color at its vertices
-        # interpolating smoothly the color in the interior
+        if (self.m_model == None) or (self.m_model.isEmpty()): return
+        glCallList(self.list)
+        glDeleteLists(self.list, 1)
+        self.list = glGenLists(1)
+        glNewList(self.list, GL_COMPILE)
         verts = self.m_model.getVerts()
-        glShadeModel(GL_SMOOTH)
-        glColor3f(0.0, 1.0, 0.0) # green
+        glColor3f(0.0, 1.0, 0.0)  # green
         glBegin(GL_TRIANGLES)
         for vtx in verts:
             glVertex2f(vtx.getX(), vtx.getY())
         glEnd()
+        glEndList()
 
-    def setModel(self,_model):
+    def setModel(self, _model):
         self.m_model = _model
 
     def fitWorldToViewport(self):
-        print("fitWorldToViewport")
-
-        if self.m_model == None:
-            return
+        if (self.m_model == None) or (self.m_model.isEmpty()): return
         self.m_L, self.m_R, self.m_B, self.m_T = self.m_model.getBoundBox()
         self.scaleWorldWindow(1.10)
         self.update()
